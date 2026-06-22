30870b217737bf9b7eb71511627979416c394c07
iricehasan <hsn.irice@gmail.com>
2026-06-22 19:57:34 +0300

    feat: add comments

--- src/chapter3/HelloWorld.java
+++ src/chapter3/HelloWorld.java
@@ -1,4 +1,11 @@
+package chapter3;
+
+/*
+    multi-line comments
+*/
+
 public class HelloWorld {
+    // this is the entrypoint of the application
     public static void main(String[] args) {
         System.out.println("Hello World from Java!");
     }
